02455f77d0f1befb18e76db5187da8dc2da7558c
MarttiU <martti.umbleja@gmail.com>
2024-04-01 20:41:39 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//See on kommentaar mu koodile
+//Rakendus küsib kasutajalt sisestada tema nime
+//Rakendus tervitab nimepidi
+
+Console.WriteLine("Sisesta oma nimi");
+//string - sõne (jah sõne)
+string userName;
+userName = Console.ReadLine();
+Console.WriteLine("Tere" + ", " + userName + "!");
+
+//string interpolation
+
+Console.WriteLine($"Hello {userName}!");
